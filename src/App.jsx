7ddb580c5bb96719b0fe90dd2520b9c61a91aaa4
HoangCacLoanChau <hoangcacloanchau@gmail.com>
2025-03-17 15:03:09 +0700

    delete st

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Header from "./Components/Header";
 import Intro from "./Components/Intro";
 import Project from "./Components/Project";
 import Skill from "./Components/Skill";
-import Statis from "./Components/Statis";
 import { FloatButton } from "antd";
 
 function App() {
@@ -15,7 +14,6 @@ function App() {
       <Skill />
       <Project />
       <AboutMe />
-      <Statis />
       <Footer />
       <FloatButton.BackTop />
     </>
